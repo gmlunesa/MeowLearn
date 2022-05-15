648cedb6f6d2069a54780cfba786e0a014447679
gmlunesa <gmlunesa@gmail.com>
2022-05-15 18:46:26 +0800

    Add MediaType seed data

--- MeowLearn/Data/ApplicationDbContext.cs
+++ MeowLearn/Data/ApplicationDbContext.cs
@@ -10,32 +10,57 @@ using System.Text;
 
 namespace MeowLearn.Data
 {
-
     public class ApplicationUser : IdentityUser
     {
         [StringLength(250)]
         public string FirstName { get; set; }
+
         [StringLength(250)]
         public string LastName { get; set; }
+
         [StringLength(250)]
         public string Address { get; set; }
+
         [StringLength(20)]
         public string ZipCode { get; set; }
 
         [ForeignKey("UserId")]
-        public virtual ICollection<UserCategory> UserCategory { get; set;}
+        public virtual ICollection<UserCategory> UserCategory { get; set; }
     }
+
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        { }
 
         public DbSet<Category> Category { get; set; }
         public DbSet<UserCategory> UserCategory { get; set; }
         public DbSet<CategoryItem> CategoryItem { get; set; }
         public DbSet<MediaType> MediaType { get; set; }
         public DbSet<Content> Content { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            #region MediaTypeSeed
+            modelBuilder
+                .Entity<MediaType>()
+                .HasData(
+                    new MediaType
+                    {
+                        Id = 1,
+                        Name = "Lecture",
+                        ThumbnailImagePath = "/images/LectureImage.jpeg"
+                    },
+                    new MediaType
+                    {
+                        Id = 2,
+                        Name = "Video",
+                        ThumbnailImagePath = "/images/VideoImage.jpeg"
+                    }
+                );
+
+            #endregion
+        }
     }
 }
---
+++ MeowLearn/Data/Migrations/20220515083833_AddMediaTypeSeed.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MeowLearn.Data.Migrations
+{
+    public partial class AddMediaTypeSeed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "MediaType",
+                columns: new[] { "Id", "Name", "ThumbnailImagePath" },
+                values: new object[] { 1, "Lecture", "/images/LectureImage.jpeg" });
+
+            migrationBuilder.InsertData(
+                table: "MediaType",
+                columns: new[] { "Id", "Name", "ThumbnailImagePath" },
+                values: new object[] { 2, "Video", "/images/VideoImage.jpeg" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "MediaType",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "MediaType",
+                keyColumn: "Id",
+                keyValue: 2);
+        }
+    }
+}
