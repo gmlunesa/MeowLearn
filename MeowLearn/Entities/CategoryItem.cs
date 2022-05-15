d60a1daf38f67b167d4a6b7871a5c0e57d45e8ee
gmlunesa <gmlunesa@gmail.com>
2022-05-15 16:51:51 +0800

    Added Description field in CategoryItem entity with relevant migrations Added scaffolded controller and view for CategoryItem

--- MeowLearn/Entities/CategoryItem.cs
+++ MeowLearn/Entities/CategoryItem.cs
@@ -1,16 +1,24 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace MeowLearn.Entities
 {
     public class CategoryItem
     {
         public int Id { get; set; }
+
         [Required]
         [StringLength(200, MinimumLength = 10)]
         public string Name { get; set; }
+        public string Description { get; set; }
         public int CategoryId { get; set; }
         public int MediaTypeId { get; set; }
+
+        [NotMapped]
+        public virtual ICollection<SelectListItem> MediaTypes { get; set; }
         public DateTime ReleaseDate { get; set; }
     }
 }
